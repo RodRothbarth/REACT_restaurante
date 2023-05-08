c85047be9223d9ff1d4e1f0a8b5b173827242828
rodrothbarth <rodrothbarth@gmail.com>
2023-05-08 11:15:34 -0300

    solve header show on notfound page

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,8 +1,9 @@
 import styles from './prato.module.scss';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Routes, Route, useNavigate, useParams } from 'react-router-dom';
 import cardapio from 'Data/cardapio.json';
 import { NotFound } from '../notFound';
 import { TagsPratos } from '../../Components/TagsPratos';
+import BasePage from '../../Components/BasePage';
 
 export function Prato() {
 	const { id } = useParams();
@@ -12,20 +13,29 @@ export function Prato() {
 		return <NotFound />;
 	}
 	return (
-		<>
-			<button className={styles.voltar} onClick={() => navigate(-1)}>
-				{'< Voltar'}
-			</button>
-			<section className={styles.container}>
-				<h1 className={styles.titulo}>{prato.title}</h1>
-				<div className={styles.imagem}>
-					<img src={prato.photo} alt={prato.title} />
-				</div>
-				<div className={styles.conteudo}>
-					<p className={styles.conteudo__descricao}>{prato.description}</p>
-				</div>
-				<TagsPratos {...prato} />
-			</section>
-		</>
+		<Routes>
+			<Route path="*" element={<BasePage />}>
+				<Route
+					index
+					element={
+						<>
+							<button className={styles.voltar} onClick={() => navigate(-1)}>
+								{'< Voltar'}
+							</button>
+							<section className={styles.container}>
+								<h1 className={styles.titulo}>{prato.title}</h1>
+								<div className={styles.imagem}>
+									<img src={prato.photo} alt={prato.title} />
+								</div>
+								<div className={styles.conteudo}>
+									<p className={styles.conteudo__descricao}>{prato.description}</p>
+								</div>
+								<TagsPratos {...prato} />
+							</section>
+						</>
+					}
+				/>
+			</Route>
+		</Routes>
 	);
 }
